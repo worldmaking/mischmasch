18429fbd14ffd37f39bcbf25bc409ea8ffa29f5e
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2019-04-11 17:37:15 -0400

    ot scripting added to max patch

--- OTscripting.js
+++ OTscripting.js
@@ -1,6 +1,6 @@
 
 inlets = 2
-outlets = 3
+outlets = 2
 	// get a reference to "thegen"'s embedded gen patcher:
 var varnameCount = 0
 
@@ -14,7 +14,7 @@ var varnamesTable = new Array();
 var object = {};
 	var nodeName;
 
-var counter = 1;
+counter = 1;
 
 var feedbackConnections = 0
 
@@ -29,203 +29,13 @@ function client(msg){
     switch(cmd){
 
         case "deltas":
-			counter++
-			
 			var delta = new Dict("delta");
 			delta.parse(msg);
-			
-			var data = ot.data[0]
-			var op = data.op
-			var kind = data.kind
-			var posX = data.pos[0]
-			var posY = data.pos[1]
-			
-			switch (op){
-				
-				// create an object!
-				case "newnode":
-				
-					var newModule = gen_patcher.newdefault([(posX + counter) * 100, (posY + counter) * 50, kind])
-					newModule.varname = kind + "_" + data.path 
-					
-					if (kind === "outs"){
-			
-					gen_patcher.message("script", "connect", newModule.varname, 0, "dac_left", 0);
-					gen_patcher.message("script", "connect", newModule.varname, 1, "dac_right", 0);
-		
-				}
-				
-				break
-				
-				
-				
-				
-				}
-			
         break;
 
         case "patch":
-		
-			counter = 1;
-			gen_patcher = this.patcher.getnamed("world").subpatcher();
-
-				gen_patcher.apply(function(b) { 
-				
-				//post("ops: ", b.varname)
-				// prevent erasing our audio outputs from genpatcher
-				if(b.varname !== "dac_right" && b.varname !== "dac_left"){
-					
-					//post("removing: ", b.varname)
-
-					gen_patcher.remove(b); 
-					
-					}
-			});
 			var patch = new Dict("patch");
 			patch.parse(msg);
-			
-			var scene = ot.value
-			outlet(2, 'clear')
-	post(scene)
-	var arcs = JSON.stringify(scene.arcs)
-	//post("\n\narcs", arcs)
-	var nodes = JSON.stringify(scene.nodes)
-	//post("\n\nnodes", nodes)
-	var varnames = new Array()
-	//post(arcs.length,"\n\n")
-	//var a = ["a", "b", "c"];
-	
-	
-	Object.keys(scene.nodes).forEach(function(key) {
-		
-	
-		varnameCount++
-		paramCounter = 0;
-		nodeName = key;
-		var _props = scene.nodes[key]._props 
-		var kind = _props.kind
-		var pos = _props.pos								
-		var unit = scene.nodes[key]
-		var gen = JSON.stringify(unit._props.kind)		
-		var checkOp = gen.split(' ')[0];
-		var genType = kind.split("_")[0]
-			
-		// switch between gen operators, params, and @gen abstractions (eveything else)
-		switch (genType){
-			
-			case "op":
-			op = kind.split("_")[1]
-			var newModule = gen_patcher.newdefault([(pos[0] + counter) * 100, (pos[1] + counter) * 50, op])
-			newModule.varname = nodeName
-			break;
-			
-			case "param":
-			var args = _props.args
-			param = kind.split("_")[0]
-			var objSettings = [(pos[0] + counter) * 100, (pos[1] + counter) * 50, param ]
-			var paramSettings = args
-			var newParam = objSettings.concat(paramSettings);
-			var newModule = gen_patcher.newdefault(newParam)
-			newModule.varname = nodeName
-			break;
-	
-			default:			
-			var newModule = gen_patcher.newdefault([(pos[0] + counter) * 100, (pos[1] + counter) * 50, "gen", "@gen", kind])
-			newModule.varname = nodeName
-			break;
-		}
-
-		// attach all outs modules to the dac outputs. 
-		if (kind === "outs"){
-			
-			gen_patcher.message("script", "connect", nodeName, 0, "dac_left", 0);
-			gen_patcher.message("script", "connect", nodeName, 1, "dac_right", 0);
-		
-		} else if (kind === "param"){
-			// ignore gen operator-based param modules in the next section
-			} else {
-		
-		// get all the inlets and outlets (and eventually the UI params)
-		Object.keys(unit).forEach(function(key) {
-			var UI_obj = {}	
-			if(JSON.stringify(unit[key]._props) !== undefined){
-				UI = JSON.stringify(unit[key]._props.kind)
-				paramValue = parseFloat(JSON.stringify(unit[key]._props.value, 10))
-				// generate the param and subparam for each object
-				switch(UI){
-					case '"small_knob"':
-					case '"large_knob"':
-					case '"tuning_knob"':
-					case '"slider"':
-					case '"momentary"':
-					case '"n_switch"':
-					case '"led"':
-				
-					paramX = paramCounter * 150
-					// generate the subparam which the param will bind to
-					var setparam = gen_patcher.newdefault([(pos[0] + counter) * 100 + paramX, (pos[1] + counter) * 50 - 25, "setparam", key])
-					setparam.varname = nodeName + "_setparam_" + key
-					gen_patcher.message("script", "connect", setparam.varname, 0, nodeName, 0);
-				
-					// generate the param which the js script will bind to
-					var param = gen_patcher.newdefault([(pos[0] + counter) * 100 + paramX, (pos[1] + counter) * 50 - 50, "param", nodeName + "__" + key])
-					param.varname = nodeName + "_param_" + key
-					gen_patcher.message("script", "connect", param.varname, 0, setparam.varname, 0);
-				
-					//gen_patcher.message("script", "send", param.varname, paramValue);
-					outlet(1, nodeName + "__" + key, paramValue)
-					paramCounter++
-					break;	
-					}
-				index = JSON.stringify(unit[key]._props.index)
-	
-				UI_obj[key] = [UI,index]
-	
-				object[nodeName] = UI_obj;
-
-				}
-			})
-			
-			counter++
-		}
-	})
-	//post(JSON.stringify(object))
-	for (i = 0; i < scene.arcs.length; ++i) {
-		
-		var sourceOp1 = scene.arcs[i][0].split(".")
-		opName1 = sourceOp1[0]
-		opUI1 = sourceOp1[1]
-		var sourceOp2 = scene.arcs[i][1].split(".")
-		opName2 = sourceOp2[0]
-		opUI2 = sourceOp2[1]
-	
-		//post("\n",sourceOp1, sourceOp2)
-
-		var lookup1 = scene.nodes[opName1]
-		var lookup2 = scene.nodes[opName2]
-		//post(lookup2)
-		
-
-		//	post(opName, opUI, JSON.stringify(lookup[opUI]._props.index))
-		if(lookup1 !== undefined && lookup2 !== undefined){
-			//post("\n",JSON.stringify(lookup1), JSON.stringify(lookup2))
-			var index1 = parseInt(JSON.stringify(lookup1[opUI1]._props.index, 10))
-			var index2 = parseInt(JSON.stringify(lookup2[opUI2]._props.index, 10))
-		
-			//post(opName1, index1, opName2, index2)
-		
-			// if a feedback connection is made, add a history object!
-			if(opName1 === opName2){
-				feedbackConnections++
-				var history = gen_patcher.newdefault([20,20, "history"])
-				history.varname = "feedback_" + feedbackConnections
-				gen_patcher.message("script", "connect", opName1, index1, history.varname, 0);
-				gen_patcher.message("script", "connect", history.varname, 0, opName2, index2);
-			} else {
-				gen_patcher.message("script", "connect", opName1, index1, opName2, index2);
-			}
-		}
-	}
 
         break;
 
@@ -245,7 +55,8 @@ function client(msg){
     }
 	
 	// var jsonString = JSON.stringify(jsonObjectFromSomewhere);
-
+	var d = new Dict("ot");
+	d.parse(msg);
 
 //	var cmd = JSON.stringify(msg.cmd)
 
