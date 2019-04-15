32fc774f5e23f7685aa878e523571f9cebb81095
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2019-04-15 15:56:30 -0400

    i think all of the gen~ code is ready

--- msvr_world.js
+++ msvr_world.js
@@ -29,7 +29,6 @@ var handleDelta = function(delta) {
 			// create an object!
 			case "newnode": 
 				// individual delta to handle:
-				counter++;
 				paramCounter = 0;
 				
 				var kind = delta.kind
@@ -39,214 +38,227 @@ var handleDelta = function(delta) {
 				var posY = 10
 				if (delta.pos) {
 
-				
+					counter++
 					posX = (delta.pos[0] + 3)
 					posY = (delta.pos[1] + 3) 
 					
-				var newModule = gen_patcher.newdefault([(posX) * 300, (posY) * 250, kind])
-				newModule.varname = delta.path.split('.')[0]
-
-				// if kind is outs, connect its outlets to the out1 and out2 in gen~ world
-				
-				if (kind === "outs"){
-		
-					gen_patcher.message("script", "connect", newModule.varname, 0, "dac_left", 0);
-					gen_patcher.message("script", "connect", newModule.varname, 1, "dac_right", 0);
-				}
-				} else {
-					
-					switch(kind){
-						case 'small_knob':
-						case 'large_knob':
-						case 'tuning_knob':
-						case 'slider':
-						case 'momentary':
-						case 'n_switch':
-						case 'led':
-						//post(kind, delta.path)
+					switch(delta.category){
 						
-					nodeName = delta.path.split('.')[0]
-					paramName = delta.path.replace('.','__')
-					setparamName = delta.path.split('.')[1]
-					//post(nodeName)
+						case "abstraction":
+						
+						break;
+						
+						case "operator":
+						
+						break;
+						
+						default:
+						
+						break;	
+					}
 					
-					paramX = paramCounter * 150
-					// generate the subparam which the param will bind to
-					var setparam = gen_patcher.newdefault([posX * 100 + paramX, posY * 50 - 25, "setparam", setparamName])
-					setparam.varname = 'setparam_' + paramName
-					gen_patcher.message("script", "connect", setparam.varname, 0, nodeName, 0);
-				
-					// generate the param which the js script will bind to
-					var param = gen_patcher.newdefault([(posX + counter) * 100 + paramX, (posY + counter) * 50 - 50, "param", paramName])
-					param.varname = paramName
-					gen_patcher.message("script", "connect", param.varname, 0, setparam.varname, 0);
-				
-					//gen_patcher.message("script", "send", param.varname, paramValue);
-					//post('\n\n', delta.value)
-					outlet(1, paramName, delta.value, delta.range)
-					paramCounter++
-					
-					break;
-					
-					case "inlet": 
-					
-					object[delta.path.replace('.','__')] = delta.index
-					inletsTable.push(object)
-					
-					//post(JSON.stringify(inletsTable))
-					case "outlet":
-					//post('found ', kind)
-					object[delta.path.replace('.','__')] = delta.index
-					outletsTable.push(object)
-					
-					//post(JSON.stringify(outletsTable))
-					break;
-						// TEMP HACK!!!!
-					// so we can ignore UI objects that we don't need to patcher script at this point
-					// NEED TO FIX
+					var newModule = gen_patcher.newdefault([125, counter * 25, kind])
+					newModule.varname = delta.path.split('.')[0]
 
-					// handle "inlet", "outlet", and "small_knob" etc here
-					// you need to cache them somehwere, even though they don't exist as objects in a patcher
-					// so we can know how to connect to them or change their values
+					// if kind is outs, connect its outlets to the out1 and out2 in gen~ world
+					
+					if (kind === "outs"){
+			
+						gen_patcher.message("script", "connect", newModule.varname, 0, "dac_left", 0);
+						gen_patcher.message("script", "connect", newModule.varname, 1, "dac_right", 0);
+					}
+					} else {
+					
+						switch(kind){
+							case 'small_knob':
+							case 'large_knob':
+							case 'tuning_knob':
+							case 'slider':
+							case 'momentary':
+							case 'n_switch':
+							case 'led':
+							//post(kind, delta.path)
+						
+							nodeName = delta.path.split('.')[0]
+							paramName = delta.path.replace('.','__')
+							setparamName = delta.path.split('.')[1]
+							//post(nodeName)
+							
+							paramX = paramCounter * 150
+							// generate the subparam which the param will bind to
+							var setparam = gen_patcher.newdefault([275, counter * 25, "setparam", setparamName])
+							setparam.varname = 'setparam_' + paramName
+							gen_patcher.message("script", "connect", setparam.varname, 0, nodeName, 0);
+						
+							// generate the param which the js script will bind to
+							var param = gen_patcher.newdefault([450, counter * 25, "param", paramName, delta.value])
+							param.varname = paramName
+							gen_patcher.message("script", "connect", param.varname, 0, setparam.varname, 0);
+						
+							//gen_patcher.message("script", "send", param.varname, paramValue);
+							//post('\n\n', delta.value)
+							outlet(1, paramName, delta.value, delta.range)
+							paramCounter++
+							
+							break;
+							
+							case "inlet": 
+					
+							object[delta.path.replace('.','__')] = delta.index
+							inletsTable.push(object)
+							
+							//post(JSON.stringify(inletsTable))
+							case "outlet":
+							//post('found ', kind)
+							object[delta.path.replace('.','__')] = delta.index
+							outletsTable.push(object)
+							
+							//post(JSON.stringify(outletsTable))
+							break;
+								// TEMP HACK!!!!
+							// so we can ignore UI objects that we don't need to patcher script at this point
+							// NEED TO FIX
 
-				}		
+							// handle "inlet", "outlet", and "small_knob" etc here
+							// you need to cache them somehwere, even though they don't exist as objects in a patcher
+							// so we can know how to connect to them or change their values
+
+						}		
 								//	post(kind, delta.path,"\n")
 
 								//	post(Object.keys(delta),"\n")
 									
-				for (var k in delta){
-					if (delta.hasOwnProperty(k)) {
-						//post("\n\n",k, delta[k])
-						switch (k){
-							case 'path':
-							
-							break;
-							
-							case 'range':
-							
-							outlet(4, delta.path.replace('.','__'), delta.value, delta.range)
-							break;
-							
-							case 'value':
-							
-							break;
-							
-							case 'taper':
-							
-							break;
-							
-							default:
-							
-							
-							break;
-							
-							
+						for (var k in delta){
+							if (delta.hasOwnProperty(k)) {
+								//post("\n\n",k, delta[k])
+								switch (k){
+									case 'path':
+									
+									break;
+									
+									case 'range':
+									
+									outlet(4, delta.path.replace('.','__'), delta.value, delta.range)
+									break;
+									
+									case 'value':
+									
+									break;
+									
+									case 'taper':
+									
+									break;
+									
+									default:
+									
+									
+									break;
+									
+									
+								}
 							}
-						//	post("Key is " + k + ", value is" + delta[k]);
+						}
 					}
-				}
-				
-	
-					}
-				/*var args = _props.args
-				param = kind.split("_")[0]
-				var objSettings = [(pos[0] + counter) * 100, (pos[1] + counter) * 50, param ]
-				var paramSettings = args
-				var newParam = objSettings.concat(paramSettings);
-				var newModule = gen_patcher.newdefault(newParam)
-				newModule.varname = nodeName
-				*/
-				//return;
-
-				
-
-			 break;
+			break;
 			
+			// delete an object
 			case "delnode":
 				var deleted = delta.path.replace('.', '__');
 				gen_patcher.message("script", "delete", deleted)
 			break;
-			case "disconnect":
-				
+
+			// create a patchcord!
+			case "connect": 			
 				var setOutlet = delta.paths[0].replace('.','__')
 				var setInlet = delta.paths[1].replace('.','__')
 				var input;
 				var output;
-    				for (var i = 0; i < inletsTable.length; i++) {
-						var inletsIndexes = inletsTable[i]
-        				input = JSON.stringify(inletsIndexes[setInlet]);
-        				//Do something
-    				}
-    				for (var i = 0; i < outletsTable.length; i++) {
-						var outletsIndexes = outletsTable[i]
-        				output = JSON.stringify(outletsIndexes[setOutlet]);
-        				//Do something
-    				}
+					for (var i = 0; i < inletsTable.length; i++) {
+					var inletsIndexes = inletsTable[i]
+							input = JSON.stringify(inletsIndexes[setInlet]);
+					}
+					for (var i = 0; i < outletsTable.length; i++) {
+					var outletsIndexes = outletsTable[i]
+							output = JSON.stringify(outletsIndexes[setOutlet]);
+					}
+				var selfPatch1 = delta.paths[0].split('.')[0]
+				
+				// detect a self-patch connection and insert a history object in between!
+				if (delta.paths[0].split('.')[0] === delta.paths[1].split('.')[0]){					
+					feedbackConnections++
+					var history = gen_patcher.newdefault([150,10, "history"])
+					history.varname = "feedback_" + feedbackConnections
+					gen_patcher.message("script", "connect", delta.paths[0].split('.')[0], parseInt(output), history.varname, 0);
+					gen_patcher.message("script", "connect", history.varname, 0, delta.paths[1].split('.')[0], parseInt(input));
 
-				//post(setOutlet)
+				} else {
+					// if not self-patch connection exists, just connect them. 
+					gen_patcher.message("script", "connect", delta.paths[0].split('.')[0], parseInt(output), delta.paths[1].split('.')[0], parseInt(input));
+				}
+			break;
+
+			case "disconnect":
+				var setOutlet = delta.paths[0].replace('.','__')
+				var setInlet = delta.paths[1].replace('.','__')
+				var input;
+				var output;
+				for (var i = 0; i < inletsTable.length; i++) {
+					var inletsIndexes = inletsTable[i]
+					input = JSON.stringify(inletsIndexes[setInlet]);
+				}
+				for (var i = 0; i < outletsTable.length; i++) {
+					var outletsIndexes = outletsTable[i]
+					output = JSON.stringify(outletsIndexes[setOutlet]);
+				}
 				gen_patcher.message("script", "disconnect", delta.paths[0].split('.')[0], parseInt(output), delta.paths[1].split('.')[0], parseInt(input));
-			
 			break;
 			
-			case "connect": 
-				//post('newconnection: ', delta.paths[0])
-				
-				//need to get indexes from path first!
-				//delta.path.split('.')[0]
-				//post(delta.paths[0].split('.')[0])
-				
-				var setOutlet = delta.paths[0].replace('.','__')
-				var setInlet = delta.paths[1].replace('.','__')
-				var input;
-				var output;
-    				for (var i = 0; i < inletsTable.length; i++) {
-						var inletsIndexes = inletsTable[i]
-        				input = JSON.stringify(inletsIndexes[setInlet]);
-        				//Do something
-    				}
-    				for (var i = 0; i < outletsTable.length; i++) {
-						var outletsIndexes = outletsTable[i]
-        				output = JSON.stringify(outletsIndexes[setOutlet]);
-        				//Do something
-    				}
-
-				//post(setOutlet)
-				gen_patcher.message("script", "connect", delta.paths[0].split('.')[0], parseInt(output), delta.paths[1].split('.')[0], parseInt(input));
-
-				post("script", "connect", delta.paths[0].split('.')[0], output, delta.paths[1].split('.')[0], input, '\n\n')
-
-				// connect delta.paths[0] to delta.paths[1]
-			 break;
-			case "propchange": {
+			// modify a parameter
+			case "propchange": 
 				// special case name == pos, name == orient, name == value
 				switch(delta.name) {
-					case "value": {
+					case "value": 
 						var param = delta.path
 						param = param.replace(".", "__")
 						outlet(3, delta.to, param)
 						// handle knob twiddle
 						// send to appropriate param
 						// based on delta.path and delta.to (new value)
-					} break;
-					case "pos": {
+					break;
+					
+					case "pos": 
 						// whatever
-					} break;
+					break;
 				}
-			} break;
+			break;
 			// ETC
 		} 
 	}
 }
 
 function client(msg){
-	post(msg)
+
+	//post(msg)
 	var ot = JSON.parse(msg)
 	outlet(1, ot.cmd)
 	cmd = ot.cmd
 
 	switch(cmd){
+		case "clear_world":
+			gen_patcher = this.patcher.getnamed("world").subpatcher();
 
+			gen_patcher.apply(function(b) { 
+			
+				// prevent erasing our audio outputs from genpatcher
+				if(b.varname !== "dac_right" && b.varname !== "dac_left"){
+					gen_patcher.remove(b); 		
+				}
+			});
+
+		break;
 		case "deltas": {
+		counter++;
+
 			//var delta = new Dict("delta");
 			//delta.parse(msg);
 
@@ -263,14 +275,9 @@ function client(msg){
 
 			gen_patcher.apply(function(b) { 
 			
-			//post("ops: ", b.varname)
 			// prevent erasing our audio outputs from genpatcher
-			if(b.varname !== "dac_right" && b.varname !== "dac_left"){
-				
-				//post("removing: ", b.varname)
-
-				gen_patcher.remove(b); 
-				
+				if(b.varname !== "dac_right" && b.varname !== "dac_left"){
+					gen_patcher.remove(b); 		
 				}
 			});
 		var patch = new Dict("patch");
@@ -605,88 +612,14 @@ function read(file){
 	}
 }
 	
-function clear(){
-	
+function clear(){	
 	counter = 1;
-		gen_patcher = this.patcher.getnamed("world").subpatcher();
-
-			gen_patcher.apply(function(b) { 
-				
-			//post("ops: ", b.varname)
-				// prevent erasing our audio outputs from genpatcher
-				if(b.varname !== "dac_right" && b.varname !== "dac_left"){
-					
-					//post("removing: ", b.varname)
-
-					gen_patcher.remove(b); 
-					
-					}
-			});
-				
-				}
-
-/* some of this other code below is legacy from the gen scripting example. 
-some of it may prove useful for the OT interpretation>>>scripting
-				
-function module(module){
-	var gen_patcher = this.patcher.getnamed("world").subpatcher();
-
-
-	var newModule = gen_patcher.newdefault([20, 120, "gen", "@gen", module]);
-		newModule.varname = module
-
-	
-	}
-	
-	
-function op(op){
-	var gen_patcher = this.patcher.getnamed("world").subpatcher();
-
-		gen_patcher.apply(function(b) { 				
-
-			if(b.varname !== "dac_right" && b.varname !== "dac_left"){
-					
-					//post("removing: ", b.varname)
-
-				gen_patcher.remove(b); 
-					
-				} 
-			});
-
-
-	var newModule = gen_patcher.newdefault([20, 120, op]);
-	
-	}
-function bang() {
-	
-	
-	// remove all the existing objects:
-	gen_patcher.apply(function(b) { gen_patcher.remove(b); });
-
-	// create a couple of objects:
-	var out1_box = gen_patcher.newdefault([20, 120, "out", 1]);
-	var osc_box = gen_patcher.newdefault([20, 20, "cycle", 1000*Math.random()*Math.random()]);
-	
-	// and connect them
-	gen_patcher.connect(osc_box, 0, out1_box, 0);
+	gen_patcher = this.patcher.getnamed("world").subpatcher();
+	gen_patcher.apply(function(b) { 
+		// prevent erasing our audio outputs from genpatcher
+		if(b.varname !== "dac_right" && b.varname !== "dac_left"){
+		gen_patcher.remove(b); 				
+		}
+	});			
 }
 
-function biggerer() {
-	
-	// get a reference to "thegen"'s embedded gen patcher:
-	var gen_patcher = this.patcher.getnamed("world").subpatcher();
-		
-	// remove all the existing objects:
-	gen_patcher.apply(function(b) { gen_patcher.remove(b); });
-	
-	var out1_box = gen_patcher.newdefault([20, 400, "out", 1]);
-		
-	var mul_box = gen_patcher.newdefault([20, 360, "*", 0.1]);
-	gen_patcher.connect(mul_box, 0, out1_box, 0);
-	
-	for (var i=0; i<10; i++) {
-		var osc_box = gen_patcher.newdefault([20 + i*50, 20 + i*20, "cycle", 1000*Math.random()*Math.random()]);
-		gen_patcher.connect(osc_box, 0, mul_box, 0);
-	}
-}
-*/
