fa13fb99fbc10ec100f78c1b301b5cd7fe0ac2f3
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2019-08-21 17:02:54 -0400

    removed breaking changes due to the signal server

--- server.js
+++ server.js
@@ -190,27 +190,6 @@ vorpal
 
 // const signalServer = new WebSocket('ws://msvrss.appspot.com/8080')
 
-const signalServer = new WebSocket('ws://localhost:8083')
-
-signalServer.on('open', function open() {
-  let newServer = JSON.stringify({
-    cmd: 'newServer',
-    data: {
-      hostname: hostname(),
-      ip: ip.address(),
-    },
-    date: Date.now() 
-  })
-  signalServer.send(newServer);
-  //process.exit()
-});
- 
-signalServer.on('message', function incoming(message) {
-  msg = JSON.parse(message)
-  cmd = msg.cmd
-    //////////// filter messages for just this peer:
-
-});
 
 	//////////////////
 
