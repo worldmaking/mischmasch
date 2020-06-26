5a5c687f70934a3e1d41a0a7e1da862a6645e44d
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-06-26 11:52:47 -0400

    p2p working

--- clientWS.js
+++ clientWS.js
@@ -2,6 +2,9 @@ const WebSocket = require('ws')
 const wrtc = require('wrtc');
 const Coven = require('coven');
 const ws = require('ws');
+const path = require('path')
+const filename = path.basename(__filename)
+let coven;
 
 function serverConnect(){
 
@@ -14,7 +17,7 @@ function serverConnect(){
         socket.send(JSON.stringify({ cmd:"get_scene"})) 
     }
     socket.onerror = (error) => {
-    console.error(`WebSocket error: ${error}`)
+    console.error(JSON.stringify(error))
     }
     socket.onclose = function(e) {
         socket = null;
@@ -56,17 +59,27 @@ switch (msg.cmd) {
     //     vrContextID = msg.data.vrContext
     //     audioContextID = msg.data.audioContext
     // }
+    // the p2p won't run until the app.js passes along who is running as host:
     case "p2pSignalServer":
         let ip = msg.data.ip
         let port = msg.data.port
-        const coven = new Coven({ ws, wrtc, signaling: 'ws://' + ip + ':' + port });
+        coven = new Coven({ ws, wrtc, signaling: 'ws://' + ip + ':' + port });
         coven
             .on('message', ({ peerId, message }) => console.log(`${peerId}: ${message}`))
             .on('connection', pid => {
                 console.log(pid, coven.activePeers);
-                coven.sendTo(pid, 'Meow');
+                
+                // we use the filename var to route which client should get what message. alternately, could create a 2nd datachannel...
+                let p2pMsg = JSON.stringify({
+                    cmd: 'hello',
+                    source: filename,
+                    message: 'meow'
+                })
+                coven.sendTo(pid, p2pMsg);
             })
-            .on('error', console.error);
+            .on('error', () =>{
+                JSON.parse(console.error)
+            });
 
     break;
     default:
