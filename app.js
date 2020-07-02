247a093f47a313b3dac1576c5d3587a6381b4900
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-07-02 12:42:49 -0400

    last commit before branching to a host.js for the heroku host()

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-var natman = require('natman-api');
- 
-var privatePort = 8081; //The port on your machine that you want to forward
-var publicPort = 8081;  //The port you want to open to the rest of the world.
-// var protocol = 'TCP';   //The protocol you want to open with can be 'UDP' or 'TCP' (default)
- 
-natman(privatePort, publicPort, function(err, info){
-  if (err) {
-    console.error(err);
-  }
-  console.log(info);
-});
-
 /*
   Goal:
 
--- nat.js
+++ nat.js
@@ -1,19 +1,64 @@
-var natman = require('natman-api');
- 
-var privatePort = 8081; //The port on your machine that you want to forward
-var publicPort = 8081;  //The port you want to open to the rest of the world.
-// var protocol = 'TCP';   //The protocol you want to open with can be 'UDP' or 'TCP' (default)
- 
-natman(privatePort, publicPort, function(err, info){
-  if (err) {
-    console.error(err);
-  }
-  console.log(info);
-});
+// const pmp = require('pmp');
+// const defaultGateway = require('default-gateway');
+// const {gateway, interface} = defaultGateway.v4.sync();
 
-// var natman = require('natman-api');
+// // pmp.getExternalAddress('192.168.2.1',function(err,rslt){
+// //     console.log(err,rslt);
+// // });
+// console.log(gateway)
+// pmp.portMap(gateway,8081,8081,3600,function(err,rslt){1
+//   console.log(err,rslt, result.ttl);
+// });
+
+// // create a "client" instance connecting to your local gateway
+// var client = natpmp.connect('10.0.1.1');
+
+
+// // explicitly ask for the current external IP address
+// client.externalIp(function (err, info) {
+//   if (err) throw err;
+//   console.log('Current external IP address: %s', info.ip.join('.'));
+// });
+
+
+// // setup a new port mapping
+// client.portMapping({ private: 22, public: 2222, ttl: 3600 }, function (err, info) {
+//   if (err) throw err;
+//   console.log(info);
+//   // {
+//   //   type: 'tcp',
+//   //   epoch: 8922109,
+//   //   private: 22,
+//   //   public: 2222,
+//   //   ...
+//   // }
+// });
+const natUpnp = require('nat-upnp')
+var client = natUpnp.createClient();
  
-// var privatePort = 8081; //The port on your machine that you want to forward
-// var publicPort = 8081 //The port you want to open to the rest of the world.
+client.portMapping({
+  public: 8081,
+  private: 8081,
+  ttl: 10
+}, function(err) {
+  // Will be called once finished
+  console.log('finished', err)
+});
  
-// natman(privatePort, publicPort);
+// client.portUnmapping({
+//   public: 12345
+// });
+ 
+client.getMappings(function(err, results) {
+  console.log( err, results)
+});
+ 
+client.getMappings({ local: true }, function(err, results) {
+  console.log( err, 'mappings:', results)
+
+});
+ 
+client.externalIp(function(err, ip) {
+  console.log( err, 'ip:', ip)
+
+});
