662fcac9492cc8d84f629b83e90b085a00fa4bad
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-07-10 12:05:29 -0400

    remove vr and worldmaking_packages from preinstall

--- preinstall.js
+++ preinstall.js
@@ -19,34 +19,5 @@ if (fs.existsSync(__dirname + '/gotlib')) {
 
 }
 
-if (fs.existsSync(__dirname + '/max-msp/Max_Worldmaking_Package')) {
-    // Do something
-    console.log(__dirname + '/Max_Worldmaking_Package already installed')
-} else {
-    console.log('installing max package: Max Worldmaking Package')
-
-    exec('git submodule add -b devel https://github.com/worldmaking/Max_Worldmaking_Package.git', {cwd: __dirname + '/max-msp'}, (stdout,stderr,err) =>{
-    console.log(stdout, stderr, err)
-    })
-
-    exec('git submodule init', (stdout,stderr,err) =>{
-        console.log(stdout, stderr, err)
-    })
-}
-
-if (fs.existsSync(__dirname + '/max-msp/vr')) {
-    // Do something
-    console.log(__dirname + '/vr  already installed')
-} else {
-    console.log('installing max package: vr')
-    exec('git submodule add --force -b master https://github.com/worldmaking/vr.git', {cwd: __dirname + '/max-msp'}, (stdout,stderr,err) =>{
-    console.log(stdout, stderr, err)
-    })
-
-    exec('git submodule init', (stdout,stderr,err) =>{
-        console.log(stdout, stderr, err)
-    })
-}
-
 
 
