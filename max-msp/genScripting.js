2ddc3c73b67398e796898c4172139556df900455
Zodsmar <zodsmar@gmail.com>
2020-07-14 16:24:02 -0400

    fixed the error with certain areas of js firing before maxpat finished loading.

--- max-msp/genScripting.js
+++ max-msp/genScripting.js
@@ -28,6 +28,10 @@ var newModule;
 var speakerTable = []
 var genOutCounter = 1
 
+var audiovizBuffer = new Buffer("audioviz");
+
+var getAudioVizErrorDirty = 0
+
 // we use these vars for the visualization of gen audio in the local vr client
 var audiovizLookup = new Object()
 var audiovizIndex = 0
@@ -39,7 +43,7 @@ var namespace = new Dict("namespace")
 
 var speakerTableDict = new Dict("speakerTableDict");
 
-outlet(6, 'genScriptingReloaded')
+
 
 function loadbang(){
 	
@@ -47,7 +51,6 @@ function loadbang(){
 	gen_patcher = this.patcher.getnamed("world").subpatcher();
 
 	initiate()
-
 }
 function initiate(){
 
@@ -247,15 +250,6 @@ var handleDelta = function(delta) {
 									param.varname = paramName
 									gen_patcher.message("script", "connect", param.varname, 0, setparam.varname, 0);
 								
-									//gen_patcher.message("script", "send", param.varname, paramValue);
-									// var namespace = {}
-
-									// namespace[paramName]['value'] = delta.value
-									// namespace[paramName]['min'] = delta.range[0]	
-									// namespace[paramName]['max'] = delta.range[1]	
-									// namespace.setparse(paramName, '{ "value" : delta.value }')	
-									// namespace.replace(paramName + "::min", delta.range[0])
-									// namespace.replace(paramName + "::max", delta.range[1])			
 									outlet(1, paramName, delta.range)
 									paramCounter++
 								
@@ -399,13 +393,6 @@ var handleDelta = function(delta) {
 							}
 						}
 					});
-					
-
-				
-					/*
-					var deleteSetParam = 'setparam_' + deleted
-					gen_patcher.message("script", "delete", deleted)
-					gen_patcher.message("script", "delete", deleteSetParam)*/
 
 				} else {
 					// error. received a delnode for a nonexistent node
@@ -508,29 +495,6 @@ var handleDelta = function(delta) {
 		} 
 	}
 }
-/*
-// this is only for working within max
-function clear(){
-	bufferChannelCounter = 0;
-	bufferChannelPaths = []	
-	counter = 1;
-	speakerNumber = 1
-	feedbackConnections = 0
-	speakerTable.length = 0
-	gen_patcher = this.patcher.getnamed("world").subpatcher();
-	gen_patcher.apply(function(b) { 
-		// prevent erasing our audio outputs from genpatcher
-		if(b.varname !== "visualFeedbackBuffer" && b.varname !== "bufferChannels" && b.varname !== "PLO"){
-		gen_patcher.remove(b); 				
-		}
-	});		
-			inletsTable = [];
-			outletsTable = [];
-
-			//store varnames per node
-			varnamesTable = [];	
-}
-*/
 
 function fromLocalWebsocket(msg){
 	
@@ -552,286 +516,12 @@ function fromLocalWebsocket(msg){
 			initiate()
 		break
 
-			//? this case isn't likely necessary, but the code in it might be useful elsewhere
-		// case "clear_scene":
-		// 	outlet(0, 'clear_scene')
-		// 	for (i = 0; i < speakerTable.length; i++){
-		// 		outlet(3, 'script', 'delete', speakerTable[i])
-
-		// 	}
-		// 	// vizBuffers = new Array()
-		// 	// bufferChannelCounter = 0;
-		// 	// bufferChannelPaths = [];
-		// 	speakerNumber = 1
-		// 	speakerTable.length = 0
-		// 	gen_patcher = this.patcher.getnamed("world").subpatcher();
-		// 	//bufferStorage = this.patcher.getnamed("bufferStorage").subpatcher();
-
-		// 	gen_patcher.apply(function(b) { 
-			
-		// 		// prevent erasing our audio outputs from genpatcher
-		// 		if(b.varname !== "PLO"){
-		// 			gen_patcher.remove(b); 		
-		// 		}
-		// 	});
-
-
-		// 	// bufferStorage.apply(function(b) { 
-			
-		// 	// 		gen_patcher.remove(b); 		
-				
-		// 	// });
-			
-		// 	inletsTable = [];
-		// 	outletsTable = [];
-
-		// 	//store varnames per node
-		// 	varnamesTable = [];
-			
-
-		// 	object = {};
-		// 	pathName;
-		// 	counter = 1;
-		// 	feedbackConnections = 0	
-
-		// break;
-
-			
-		//? Don't think the message 'patch' is going to come through here...
-		// case "patch":
-	
-		// 	counter = 1;
-		// 	gen_patcher = this.patcher.getnamed("world").subpatcher();
-
-		// 	gen_patcher.apply(function(b) { 
-			
-		// 	// prevent erasing our audio outputs from genpatcher
-		// 		if(b.varname !== "visualFeedbackBuffer" && b.varname !== "bufferChannels" && b.varname !== "PLO"){
-		// 			gen_patcher.remove(b); 		
-		// 		}
-		// 	});
-		// var patch = new Dict("patch");
-		// patch.parse(msg);
-		
-		// var scene = ot.value
-		// var arcs = JSON.stringify(scene.arcs)
-		// var nodes = JSON.stringify(scene.nodes)
-		// var varnames = new Array()
-		// //var a = ["a", "b", "c"];
-	
-	
-	// Object.keys(scene.nodes).forEach(function(key) {
-		
-	
-	// 	varnameCount++
-	// 	paramCounter = 0;
-	// 	pathName = key;
-	// 	var _props = scene.nodes[key]._props 
-	// 	var kind = _props.kind
-	// 	var pos = _props.pos								
-	// 	var unit = scene.nodes[key]
-	// 	var gen = JSON.stringify(unit._props.kind)		
-	// 	var checkOp = gen.split(' ')[0];
-	// 	var genType = kind.split("_")[0]
-			
-	// 	// switch between gen operators, params, and @gen abstractions (eveything else)
-	// 	switch (genType){
-			
-	// 		case "op":
-	// 		op = kind.split("_")[1]
-	// 		var newModule = gen_patcher.newdefault([(pos[0] + counter) * 100, (pos[1] + counter) * 50, op])
-	// 		newModule.varname = pathName
-	// 		break;
-			
-	// 		case "param":
-	// 		var args = _props.args
-	// 		param = kind.split("_")[0]
-	// 		var objSettings = [(pos[0] + counter) * 100, (pos[1] + counter) * 50, param ]
-	// 		var paramSettings = args
-	// 		var newParam = objSettings.concat(paramSettings);
-	// 		var newModule = gen_patcher.newdefault(newParam)
-	// 		newModule.varname = pathName
-	// 		break;
-	
-	// 		default:			
-	// 		var newModule = gen_patcher.newdefault([(pos[0] + counter) * 100, (pos[1] + counter) * 50, "gen", "@gen", kind])
-	// 		newModule.varname = pathName
-	// 		break;
-	// 	}
-
-	// 	// create a new outlet in gen~ world for each added speaker
-	// 	if (kind === "speaker"){
-	// 					// // create the speaker aka gen [out #]
-	// 					// var newSpeaker = gen_patcher.newdefault([(pos[0] + counter), (pos[1] + counter) * 150, 'out', speakerNumber])
-	// 					// newSpeaker.varname = 'speaker_' + speakerNumber
-	// 					// // add a vr.Source~ abstraction to parent, script the new out to this abstraction. 
-	// 					// var vrSource = this.patcher.newdefault([(pos[0] + counter), (pos[1] + counter) * 150, "vr.source~", speakerNumber - 1, "@varname", "source_" + speakerNumber])
-						
-	// 					// this.patcher.message("script", "connect", 'world',  "speaker_" + speakerNumber - 1,  "source_" + speakerNumber, 0);
-
-
-	// 					// // need to get its position in vr and apply that to a vr.source~ position
-
-	// 					// speakerNumber++
-	// 	} else if (kind === "param"){
-	// 		// ignore gen operator-based param modules in the next section
-	// 		} else if(kind === "controller1" || kind === "controller2" || kind === "headset"){
-	// 				paramX = paramCounter * 150
-	// 				// generate the subparam which the param will bind to
-	// 				var setparam = gen_patcher.newdefault([(pos[0] + counter) * 100 + paramX, (pos[1] + counter) * 50 - 25, "setparam", key])
-	// 				setparam.varname = pathName + "_setparam_" + key
-	// 				gen_patcher.message("script", "connect", setparam.varname, 0, pathName, 0);
-				
-	// 				// generate the param which the js script will bind to
-	// 				var param = gen_patcher.newdefault([(pos[0] + counter) * 100 + paramX, (pos[1] + counter) * 50 - 50, "param", kind + "__" + key])
-	// 				param.varname = pathName + "_param_" + key
-	// 				gen_patcher.message("script", "connect", param.varname, 0, setparam.varname, 0);
-				
-	// 				//gen_patcher.message("script", "send", param.varname, paramValue);
-	// 				//outlet(1, kind + "__" + key, paramValue)
-	// 				paramCounter++
-	// 		} else {
-		
-	// 	// get all the inlets and outlets (and eventually the UI params)
-	// 	Object.keys(unit).forEach(function(key) {
-	// 		var UI_obj = {}	
-	// 		if(JSON.stringify(unit[key]._props) !== undefined){
-	// 			UI = JSON.stringify(unit[key]._props.kind)
-	// 			paramValue = parseFloat(JSON.stringify(unit[key]._props.value, 10))
-	// 			// generate the param and subparam for each object
-	// 			switch(UI){
-	// 				case '"small_knob"':
-	// 				case '"large_knob"':
-	// 				case '"tuning_knob"':
-	// 				case '"slider"':
-	// 				case '"momentary"':
-	// 				case '"n_switch"':
-	// 				case '"led"':
-				
-	// 				paramX = paramCounter * 150
-	// 				// generate the subparam which the param will bind to
-	// 				var setparam = gen_patcher.newdefault([(pos[0] + counter) * 100 + paramX, (pos[1] + counter) * 50 - 25, "setparam", key])
-	// 				setparam.varname = pathName + "_setparam_" + key
-	// 				gen_patcher.message("script", "connect", setparam.varname, 0, pathName, 0);
-				
-	// 				// generate the param which the js script will bind to
-	// 				var param = gen_patcher.newdefault([(pos[0] + counter) * 100 + paramX, (pos[1] + counter) * 50 - 50, "param", pathName + "__" + key])
-	// 				param.varname = pathName + "_param_" + key
-	// 				gen_patcher.message("script", "connect", param.varname, 0, setparam.varname, 0);
-				
-	// 				//gen_patcher.message("script", "send", param.varname, paramValue);
-	// 				// outlet(1, pathName + "__" + key, paramValue)
-	// 				paramCounter++
-	// 				break;	
-	// 				}
-	// 			index = JSON.stringify(unit[key]._props.index)
-	
-	// 			UI_obj[key] = [UI,index]
-	
-	// 			object[pathName] = UI_obj;
-
-	// 			}
-	// 		})
-			
-	// 		//counter++
-	// 	}
-	// })
-	// for (i = 0; i < scene.arcs.length; ++i) {
-		
-	// 	var sourceOp1 = scene.arcs[i][0].split(".")
-	// 	opName1 = sourceOp1[0]
-	// 	opUI1 = sourceOp1[1]
-	// 	var sourceOp2 = scene.arcs[i][1].split(".")
-	// 	opName2 = sourceOp2[0]
-	// 	opUI2 = sourceOp2[1]
-	
-
-	// 	var lookup1 = scene.nodes[opName1]
-	// 	var lookup2 = scene.nodes[opName2]
-		
-
-	// 	if(lookup1 !== undefined && lookup2 !== undefined){
-	// 		var index1 = parseInt(JSON.stringify(lookup1[opUI1]._props.index, 10))
-	// 		var index2 = parseInt(JSON.stringify(lookup2[opUI2]._props.index, 10))
-		
-		
-	// 		// if a feedback connection is made, add a history object!
-	// 		if(opName1 === opName2){
-	// 			feedbackConnections++
-	// 			var history = gen_patcher.newdefault([20,20, "history"])
-	// 			history.varname = "feedback_" + feedbackConnections
-	// 			gen_patcher.message("script", "connect", opName1, index1, history.varname, 0);
-	// 			gen_patcher.message("script", "connect", history.varname, 0, opName2, index2);
-	// 		} else {
-	// 			gen_patcher.message("script", "connect", opName1, index1, opName2, index2);
-	// 		}
-	// 	}
-	// }
-
-    //     break;
-	// 	/*
-    //     case "user_pose":
-
-	// 		//var pose = new Dict("pose");
-	// 		//pose.parse(msg);
-			
-	// 		var data = JSON.parse(msg)
-	// 		var pose = data.pose
-			
-	// 		// IMPORTANT: we eventually will need to filter controller data by who is using it (aka the data.pose.id). 
-	// 		var id = data.pose.id
-	// 		// TODO: filter remaining data based on ID, and add something to the VR space that allows for switching the gestural data source
-	// 		// TODO: when connecting a cable from a gestural module to a given inlet/knob, need to grab the range of the inlet/knob and insert a scale object between the cable. 
-	// 		var headPosition // do this later
-			
-	// 		// Headset
-	// 		var headData; 
-			
-	// 		// Controllers
-	// 		var controller1 = data.pose.controller1
-	// 			// c1 pos data
-	// 			outlet(1, "controller1__pos_x", data.pose.controller1.pos.x)
-	// 			outlet(1, "controller1__pos_y", data.pose.controller1.pos.y)
-	// 			outlet(1, "controller1__pos_z", data.pose.controller1.pos.z)
-	// 			// c1 orient data
-	// 			outlet(1, "controller1__orient_x", data.pose.controller1.orient.x)
-	// 			outlet(1, "controller1__orient_y", data.pose.controller1.orient.y)
-	// 			outlet(1, "controller1__orient_z", data.pose.controller1.orient.z)
-	// 			outlet(1, "controller1__orient_w", data.pose.controller1.orient.w)
-					
-	// 		var controller2 = data.pose.controller2
-	// 						// c1 pos data
-	// 			outlet(1, "controller2__pos_x", data.pose.controller2.pos.x)
-	// 			outlet(1, "controller2__pos_y", data.pose.controller2.pos.y)
-	// 			outlet(1, "controller2__pos_z", data.pose.controller2.pos.z)
-	// 			// c1 orient data
-	// 			outlet(1, "controller2__orient_x", data.pose.controller2.orient.x)
-	// 			outlet(1, "controller2__orient_y", data.pose.controller2.orient.y)
-	// 			outlet(1, "controller2__orient_z", data.pose.controller2.orient.z)
-	// 			outlet(1, "controller2__orient_w", data.pose.controller2.orient.w)
-
-
-	// 	break;
-	// 	*/
-
-    //     default:
-
-        	post("unknown msg received: ", msg)
+        default: post("unknown msg received: ", msg)
 		break;
     }
 	
-	// var jsonString = JSON.stringify(jsonObjectFromSomewhere);
-
-
-//	var cmd = JSON.stringify(msg.cmd)
-
-//	switch (
-
 }
 
-var audiovizBuffer = new Buffer("audioviz")
-
-let getAudioVizErrorDirty = 0
 function getAudioviz(){
 	// seems the audiovizLookup isn't properly instantiated at start
 	if (typeof audiovizLookup === "object"){
