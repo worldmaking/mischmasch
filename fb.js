e4bdcb00ef557d5663a60cf18bd124ceb1e034c9
Zodsmar <zodsmar@gmail.com>
2020-07-18 19:56:22 -0400

    cycle detection code

--- fb.js
+++ fb.js
@@ -1,4 +1,4 @@
-const fb = require('./feedbackPaths.js')
+const fb = require('./detectCycles.js')
 
 const fs = require('fs')
 const got = require('./gotlib/got.js')
@@ -10,7 +10,17 @@ let deltas = got.deltasFromGraph(scene, [])
 
 got.applyDeltasToGraph(localGraph, deltas)
 // console.log(localGraph)
+// list all nodes (parent and child) in the graph
+let nodes = fb.setup(localGraph)
 
-let fbs = fb.getFeedbackPaths(localGraph)
+// get list of adjacent nodes per each node in the graph
+let adjacents = fb.getAdjacents(0, nodes, localGraph)
 
-console.log(fbs)
+// reset the nodes array with only the list of parent nodes that actually have connections:
+nodes.length = 0
+nodes = Object.keys(adjacents)
+nodeCount = nodes.length
+
+let cycles = fb.visit(0, nodes, adjacents, localGraph, nodeCount)
+
+console.log(nodes, adjacents, cycles)
