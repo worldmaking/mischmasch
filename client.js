b571ea7040fc3cd26252d320d39ba6da55652c25
Graham Wakefield <grrrwaaa@gmail.com>
2020-07-18 16:13:21 -0400

    better knob twiddle mode

--- client.js
+++ client.js
@@ -294,8 +294,6 @@ const UI = {
 	updateHandStateMachine(hand, mainScene) {
 		if (!hand.mat) return; // i.e. not tracking
 
-		console.log(hand.state)
-
 		let hits = rayTestModules(mainScene.module_instances.instances, hand.pos, hand.dir)
 		hand.target = hits[0]
 		
@@ -365,36 +363,57 @@ const UI = {
 						want angle from knob to hand
 						in the knob's coordinate system
 					*/
-					let rel = vec3.create()
-					vec3.sub(rel, hand.pos, object.i_pos)
-					let iq = quat.create()
-					quat.invert(iq, object.i_quat)
-					vec3.transformQuat(rel, rel, iq);
-					vec2.normalize(rel, rel);
-					// angle of line to hand relative to knob face:
-					let angle = Math.atan2(rel[0], rel[1]);
-					let value = angle2value(angle);
-					// clamp:
-					value = Math.min(1, Math.max(0, value));
-					// immediate update for rendering:
-					object.i_value[0] = value;
-					// update prop:
-					let props = object.node._props
-					let range = props.range || [0,1];
-					let oldval = object.node._props.value;
-					let newval = range[0] + value*(range[1]-range[0]);
-					// somehow this is getting missed?
-					//props.value = newval;
-					// send propchange oldval->newval
-					let delta = { 
-                        op:"propchange", 
-                        path: object.path, 
-                        name:"value", 
-                        from: oldval, 
-                        to: newval 
+					// let rel = vec3.create()
+					// vec3.sub(rel, hand.pos, object.i_pos)
+					// let iq = quat.create()
+					// quat.invert(iq, object.i_quat)
+					// vec3.transformQuat(rel, rel, iq);
+					/*
+						Alternate mode:
+						project ray from hand to plane of knob
+						use that point rather than hand pos for relative angle
+
+						it's the point where the ray Z=0
+
+						0 = o.z + t * d.z
+						t = -o.z/d.z
+					*/
+					let dir = glutils.quat_unrotate(vec3.create(), object.i_quat, hand.dir);
+					if (dir[2]) {
+						let origin = vec3.sub(vec3.create(), hand.pos, object.i_pos);
+						glutils.quat_unrotate(origin, object.i_quat, origin);
+
+						let t = -origin[2]/dir[2]
+						let p = vec3.create()
+						vec3.scale(p, dir, t)
+						vec3.add(p, p, origin);
+						// now want angle to this p:
+						vec2.normalize(p, p)
+
+						// angle of line to hand relative to knob face:
+						let angle = Math.atan2(p[0], p[1]);
+						let value = angle2value(angle);
+						// clamp:
+						value = Math.min(1, Math.max(0, value));
+						// immediate update for rendering:
+						object.i_value[0] = value;
+						// update prop:
+						let props = object.node._props
+						let range = props.range || [0,1];
+						let oldval = object.node._props.value;
+						let newval = range[0] + value*(range[1]-range[0]);
+						// somehow this is getting missed?
+						//props.value = newval;
+						// send propchange oldval->newval
+						let delta = { 
+							op:"propchange", 
+							path: object.path, 
+							name:"value", 
+							from: oldval, 
+							to: newval 
+						}
+						outgoingDeltas.push(delta);
 					}
-					console.log(delta)
-					outgoingDeltas.push(delta);
 
 				} else {
 					hand.state = "default";
