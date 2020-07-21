7ecf397f76ff007c97cf926b9f67f8407cb2c939
Zodsmar <zodsmar@gmail.com>
2020-07-21 18:32:58 -0400

    clean up max-msp folder

--- max-msp/async.js
+++
@@ -1,8 +0,0 @@
-inlets = 1
-outlets = 1
-
-async function f() {
-    return Promise.resolve(1);
-  }
-  
-  f().then(post('foo')); // 1
--- max-msp/audiovizHandler.js
+++
@@ -1,8 +0,0 @@
-// max.addHandler('audiovizLookup', (audiovizLookup)=>{
-// 	if(localWebsocketStatus === 1){
-// 		connection.send(JSON.stringify({
-// 			cmd: "audiovizLookup",
-// 			date: Date.now(),
-// 			data: audiovizLookup
-// 		}));
-// 	}
