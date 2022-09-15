37df0f66829c668e8e4233ee327509f4db0ba9cb
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2022-09-15 15:12:33 -0400

    webXR version

--- archives/archive_NIME-2020/deferlowScript.js
+++
@@ -1,9 +0,0 @@
-// this is used for anything that needs to happen after other operations are completed (aka i don't know if max/es5 has async or proimises)
-inlets = 1
-outlets = 1
-
-function genConnect(genOutCounter, speakerVarname){
-    //post('\n\nvarname', vrSource.varname, '\n')
-    this.patcher.message("script", "connect", 'world', genOutCounter - 1, speakerVarname, 0);
-    
-}
