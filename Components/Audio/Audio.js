557e7223bf8516d46de9b16a990030706bf394ad
Graham Wakefield <grrrwaaa@gmail.com>
2022-09-26 12:10:45 -0700

    audio worker

--- Components/Audio/Audio.js
+++ Components/Audio/Audio.js
@@ -7,17 +7,42 @@ const { Worker, MessageChannel, MessagePort, isMainThread, parentPort, workerDat
 const operators = JSON.parse(fs.readFileSync(path.join(__dirname, '../Op/mischmaschOps.json')))
 const Op = require('../Op/Op.js')
 
+// load a js file as a new Worker thread:
+const worker = new Worker(path.join(__dirname, "genish_worker.js"), {
+	// argv: becomes process.argv in worker
+	// eval: true if 1st arg is a string of code rather than a filepath
+	//workerData: "hello"  // pass initial data to worker
+});
+// here's a message channel to receive messages from it:
+//const workerChannel = new MessageChannel();
 
-
+// receicve a message:
+worker.on('message', function (msg) {
+	console.log("main received message from audio:", msg)
+});
+//worker.on('error', ...);
+//worker.on('online', ...)
+worker.on('exit', (code) => {
+	console.error(`Worker stopped with exit code ${code}`)
+	//process.exit(code)
+})
 
 module.exports = {
 
 	updateGraph(doc) {
+		try {
 
+		} catch (e) {
+			console.error(e)
+		}
 	},
 
 	updateParams(doc) {
+		try {
 
+		} catch (e) {
+			console.error(e)
+		}
 	}
 
 }
--- Components/Audio/genish_worker.js
+++ Components/Audio/genish_worker.js
@@ -10,7 +10,53 @@ if (isMainThread) {
 // console.log(workerData);
 
 // Set up audio driver
-const audio = require('./audio.js');
+const audio = require('../../../node-gles3/audio.js');
 
-const gen = require("./genish.js")
+// const gen = require("./genish.js")
 audio.start()
+
+console.log("got audio", audio)
+
+
+// Now start the main loop of this audio worker thread:
+let frameIdx = 0;
+let time = 0; // in seconds
+let lasttime = 0
+function runAudioProcess() {
+	// let dt = time - lasttime // seconds since last update()
+	// lasttime = time
+	// // this is the time in the ringbuffer that has most recently been played (and is now zeroed)
+	// // so we are safe to fill the buffer up to this point:
+	// let at = audio.t
+	// let ouch = audio.outchannels
+	// let inch = audio.inchannels
+	// let secondsPerFrame = 1/audio.samplerate
+	// //console.log(at, time)
+	// // continue filling ringbuffer until we catch up to that point:
+	// while (frameIdx != at) {
+	// 	let inframe = audio.inbuffer.subarray(frameIdx*inch)
+	// 	let outframe = audio.outbuffer.subarray(frameIdx*ouch)
+	// 	// compute next output:
+	// 	let L0 = oldkernel && mixerXfade > 0 ? oldkernel.call(oldkernel)*mixerXfade : 0
+	// 	let R0 = L0
+	// 	let L = kernel ? kernel.call(kernel)*(1-mixerXfade) : 0
+	// 	let R = L 
+	// 	mixerXfade = Math.max(0, mixerXfade - mixerXfadeStep)
+	// 	// write to output:
+	// 	outframe[0] += mixerGain*(L+L0);
+	// 	outframe[1] += mixerGain*(R+R0);
+	// 	// time passes:
+	// 	time += secondsPerFrame; 
+	// 	frameIdx = (frameIdx+1) % audio.frames;
+	// }
+
+	// // play for 10 seconds:
+	// if (time > 10) {
+	// 	audio.end()
+	// 	process.exit()
+	// } else {
+		//console.log(dt)
+		setTimeout(runAudioProcess, audio.pollms/2);
+	//}
+}
+runAudioProcess();
