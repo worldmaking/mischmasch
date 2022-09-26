f8174baf668db6f299e264ba5812b9a5a5ec9061
Graham Wakefield <grrrwaaa@gmail.com>
2022-09-26 14:04:55 -0700

    modified testpatch

--- Components/Audio/Audio.js
+++ Components/Audio/Audio.js
@@ -161,6 +161,12 @@ module.exports = {
 	updateGraph(doc) {
 		try {
 			console.log("audio got new doc", JSON.stringify(doc, null, "  "))
+
+			let operations = doc2operations(doc)
+
+			worker.postMessage({ cmd: "doc", operations })
+
+			// send them to the worker:
 		} catch (e) {
 			console.error(e)
 		}
