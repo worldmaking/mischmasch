ddd4d0d614f00d7158003d1db7afe56041c44cfa
Graham Wakefield <grrrwaaa@gmail.com>
2022-09-26 14:00:06 -0700

    update

--- Components/Audio/Audio.js
+++ Components/Audio/Audio.js
@@ -87,7 +87,7 @@ function doc2operations(doc) {
 			let inputs = conns.filter(conn => conn.input == input.name).map(conn => {
 				// for any op that we used, add it to the chain:
 				if (memo[conn.src]) {
-					// if we have already visited this object, this must be a history connection
+					// if we have already generated this object, this must be a history connection
 					// TODO
 				}
 				else {
