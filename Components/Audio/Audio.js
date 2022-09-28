534618f03d057b60632622e92d840f70ae20c18a
Graham Wakefield <grrrwaaa@gmail.com>
2022-09-28 12:27:04 -0700

    voct to hz

--- Components/Audio/Audio.js
+++ Components/Audio/Audio.js
@@ -155,6 +155,28 @@ function doc2operations(doc) {
 			if (input.name == "freq") {
 				// convert v/oct to Hz
 				// var hz = 55*Math.pow(2, oct - 21/12);
+				let sub_id = makeUID("octsub_")
+				inoperations.push({
+					name: "sub",
+					uuid: id,
+					inputs: [result, 21/12],
+					outputs: [sub_id]
+				})
+				let pow_id = makeUID("octpow")
+				inoperations.push({
+					name: "sub",
+					uuid: id,
+					inputs: [2, subid],
+					outputs: [pow_id]
+				})
+				let mul_id = makeUID("hz")
+				inoperations.push({
+					name: "mul",
+					uuid: id,
+					inputs: [55, pow_id],
+					outputs: [mul_id]
+				})
+				result = mul_id
 			}
 
 			op.inputs[i] = result
