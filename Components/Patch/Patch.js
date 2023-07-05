03a81c0d0b2a9546d0a0d3638efa0cbcaab6e0f6
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2023-07-05 14:14:11 -0400

    starting to merge the webrtc client code into mischmasch

--- Components/Patch/Patch.js
+++ Components/Patch/Patch.js
@@ -2,6 +2,19 @@ const { v4: uuidv4 } = require('uuid');
 const Automerge = require('automerge')
 const _ = require('lodash');
 const replaceAll = require("replaceall");
+/**
+ * Handler of the WebRTC data channel. In its three functions (onOpen, onMessage, onClose) you
+ *     specify what to do with the data sent across the data channel over the peer connection.
+ *
+ * @param {String} ourPeerId - Peer ID of our peer
+ * @param {String} ourPeerType - Peer Type of our peer
+ * @param {Object} peer - The peer object with the useful properties below.
+ * @param {String} peer.peerId - Id of peer
+ * @param {String} peer.peerType - Type of peer
+ * @param {RTCPeerConnection} peer.rtcPeerConnection - RTC peer connection object
+ * @param {RTCDataChannel} peer.dataChannel - RTC data channel object
+ * @param {Function} peer.remove - Closes all connections and removes the peer. Automatically called when peer leaves signaling server.
+ */
 
 
 module.exports = class Patch{
@@ -116,6 +129,7 @@ module.exports = class Patch{
         this.dirty.vr = true
         this.dirty.audio.graph = true
  
+
         return op
       break
   
@@ -362,11 +376,55 @@ module.exports = class Patch{
       this.dirty.vr = true
       this.dirty.audio.graph = true
       this.dirty.speaker = false
-    }
-    
-
-    
+    }    
   }
+
+  webRTCSend(payload){
+    // eventually we'll involve automerge. but for now, just send the payload
+
+  }
+  // dataChannelHandler(ourPeerId, ourPeerType, peer) {
+  //   const peerId = peer.peerId;
+  //   const channel = peer.dataChannel;
+  
+  //   const onOpen = (event) => {
+  //       /* 
+  //           YOUR CODE HERE - This code is executed when the data channel opens.
+  //           For example, you can send data to the peer:
+  //       */
+  //       channel.send(`Hello from ${ourPeerId}`);
+                
+  //       function intervalFunc() {
+  //           channel.send('hello from hello')
+  //       }
+  //       setInterval(intervalFunc, 1500);
+  //       };
+  //   const onMessage = (event) => {
+  //       /* 
+  //           YOUR CODE HERE - This code is executed when a message is recieved from the peer.
+  //           For example, extract the data and log it to the console:
+  //       */
+  //       const { data } = event;
+  //       console.log(peerId, "says:", `"${data}"`); // put peer data inside quotation marks
+  //   };
+  //   const onClose = (event) => {
+  //       /* 
+  //           YOUR CODE HERE - This code is executed when the data channel is closed.
+  //           For example, log the closing event to the console:
+  //       */
+  //       console.log(`Channel with ${peerId} is closing `);
+  //   };
+  
+  //   channel.onopen = (event) => {
+  //       if (event.type === "open") {
+  //           console.log("Data channel with", peerId, "is open");
+  //           channel.onmessage = onMessage;
+  //           channel.onclose = onClose;
+  //           onOpen(event);
+  //       }
+  //   };
+  // }
+
 }
 
 
